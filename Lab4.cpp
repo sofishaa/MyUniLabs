8654773eb90fc3dc5e8400b591f860e80badf5a4
sofishaaa <sofiikahunkalo@gmail.com>
2023-01-21 23:52:43 +0200

    Lab4

---
+++ Lab4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+
+    string nameOfBook;
+    string nameOfAuthor;
+    string yearOfWriting;
+    string numberOfPages;
+    string originLanguage;
+    string nameOfEdition;
+    string yearOfPublish;
+
+    cout << "Назва книги: ";
+    getline(cin, nameOfBook);
+
+   cout << "Автор книги: ";
+    getline(cin, nameOfAuthor);
+
+   
+   cout << "Рiк написання: ";
+    getline(cin, yearOfWriting);
+
+    
+    cout << "Кiлькiсть сторiнок: ";
+    getline(cin, numberOfPages);
+
+    
+    cout << "Мова оригiналу: ";
+    getline(cin, originLanguage);
+
+    
+    cout << "Найменування видавництва: ";
+    getline(cin, nameOfEdition);
+
+   
+    cout << "Рiк видавництва: ";
+    getline(cin, yearOfPublish);
+
+    return 0;
+}
